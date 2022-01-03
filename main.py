33d0f369d86c04984546ab91a0a2c12f5a0c7e72
chanLik1208 <92614601+chanLik1208@users.noreply.github.com>
2022-01-03 20:21:02 +0800

    up mmc

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 nick = False
+blue = False
 
 def on_forever():
     global nick
     nick = input.button_is_pressed(Button.A)
     if nick:
-        while nick:
-            print("true")
+            print("A true")
             nick = False
     else:
-        print("false")
+        print("A false")
+    global blue
+    blue = input.button_is_pressed(Button.B)
+    if blue:
+                    print("B true")
+                    blue = False
+    else:
+                print("B false")
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,12 +1,33 @@
 let nick = false
+let blue = false
 basic.forever(function () {
+    basic.clearScreen()
     nick = input.buttonIsPressed(Button.A)
     if (nick) {
-        while (nick) {
-            console.log("true")
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # # # # #
+            # . . . #
+            # . . . #
+            `)
+        console.log("A true")
 nick = false
-        }
     } else {
-        console.log("false")
+        console.log("A false")
+    }
+    blue = input.buttonIsPressed(Button.B)
+    if (blue) {
+        basic.showLeds(`
+            # # . . .
+            # . # . .
+            # # . . .
+            # . # . .
+            # # . . .
+            `)
+        console.log("B true")
+blue = false
+    } else {
+        console.log("B false")
     }
 })
